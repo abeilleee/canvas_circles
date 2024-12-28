f8a15eff4ca4d3940565cb4638494fe465536c66
May <abeillee@bk.ru>
2024-12-28 06:25:54 +0300

    feat: add canvas object in script.js

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
-const canvas = document.getElementById('my-canvas');
-const ctx = canvas.getContext('2d');
+// const canvas = document.getElementById('my-canvas');
+// const ctx = canvas.getContext('2d');
 
-ctx.arc(100, 100, 50, 0, Math.PI * 20);
-ctx.fill();
+// ctx.arc(100, 100, 50, 0, Math.PI * 20);
+// ctx.fillStyle = 'red';
+// ctx.fill();
+
+let canvas = {
+    c: null,             //c = context
+    width: 640,
+    height: 320,
+    circles: [],
+    init() {
+        this.c = document.getElementById('my-canvas').getContext('2d');
+        console.log('init');
+    },
+    createCircle() {
+        this.circles.push({
+            x: this.width * Math.random(),
+            y: this.height * Math.random(),
+            radius: 50 * Math.random(),
+        })
+    },
+    render(){
+        for(let circle of this.circles) {
+            this.c.arc(circle.x, circle.y, circle.radius, 0, Math.PI * 2);
+            this.c.fill();
+        }
+    },
+    start() {
+        this.init();
+        this.createCircle();
+        this.render();
+    }
+}
+
+window.addEventListener('load', () => {
+    canvas.start();
+})
